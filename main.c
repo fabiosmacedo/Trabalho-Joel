877863ee4905291a2f1bcf7ab4d636159cc5e4d0
Fabio Macedo <>
2021-05-26 23:50:51 +0000

    Limpando os testes

--- main.c
+++ main.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
-int mult(int n1, int n2)
-{
-  int r;
-  r = n1 * n2;
-  return r;
-}
 
 
 
 int main(void) {
-  int res = mult(10 , 20);
-  printf("%d", res );
+
   printf("\nHello World\n");
   return 0;
 }

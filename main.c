9a8fef7c16b90ad4484137130cce6a78f848ef54
Fabio Macedo <>
2021-06-06 13:27:10 +0000

    Criando selecao

--- main.c
+++ main.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 
+
 int main()
 {
 
 /* estrutura para a seleção */
  
   /*Seleção*/
-  struct selecao
+  struct Selecao
   {
     char escalacao;
   };
 
   /*Escalacação */
-  struct escalacao
+  struct Escalacao
   {
     int numero_camisa;
     char nome[60];
   };
   
-/* ------------------ */
+/* instancia para criar a escalação  */
+struct Escalacao escalacao;
 
+/* inserindo nova escalação */
+for(int i = 0; i < 1; i++){
+scanf("%int", &escalacao.numero_camisa);
+
+fflush(stdin);
+fgets(escalacao.nome, 40, stdin);
+
+}
   
+/* mostrar a escalacao */
+
 
 
   
